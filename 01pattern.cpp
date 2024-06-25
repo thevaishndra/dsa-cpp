9d1b3e49230f6024a4860e4c4c37c8b5c46d289e
thevaishndra <chandrav306m@gmail.com>
2024-06-26 01:15:54 +0530

    Update 01pattern.cpp

--- 01pattern.cpp
+++ 01pattern.cpp
@@ -271,5 +271,25 @@ void print19(int n){
 }
 //half left diamond + half right diamond -> star, space, star
 void print20(int n){
-
+int space = 2*n-2;
+for(int i = 1; i <= 2n-1; i++){
+    int stars = i;
+    if(i > n) stars = 2*n-i;
+    //stars
+    for(int j = 1; j <= stars; j++){
+        cout<<"*";
+    }
+    //space
+    for(int j = 1; j <= space; j++){
+        cout<<" ";
+    }
+    //stars
+    for(int j = 1; j <= stars; j++){
+        cout<<"*";
+    }
+    cout<<endl;
+    if(i < n) space -= 2;
+    else space += 2;
 }
+}
+//
