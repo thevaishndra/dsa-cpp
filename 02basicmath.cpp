cf9638f6e18cb33399b1b0e500339d486261bb01
thevaishndra <chandrav306m@gmail.com>
2024-06-27 01:24:06 +0530

    Update 02basicmath.cpp

--- 02basicmath.cpp
+++ 02basicmath.cpp
@@ -11,14 +11,72 @@ void print1(int n){
         n = n/10;// n = 456/10 =45
     }
     cout<<count<<" ";
-}/*another way to do this:
+}/*optimized way 
 log base 10 (456) + 1 almost equal to 3
-int count(int n){
+Time Complexity -> O(log base 10 (N))*/
+void print2(int n){
 int count = (int)(log base 10(n)+ 1);
 return count;
-Time Complexity -> O(log base 10 (N))}*/
+}
+
 
 //Ques 2 -> Reverse a no.
-void print2(int n){
-    
+void print3(int n){
+    inr reverseN = 0;
+    while(n > 0){
+        int lastDigit = n % 10;// 456 % 10 = 6
+        n = n / 10;// 456 / 10 = 45
+        reverseN = (reverseN * 10) + lastdigit// 0* 10= 0 + 6 = 6
+    }
+    cout<<reverseN<<" ";
+}
+
+//Check palindrome no.
+//The reverse of the no. is exactly same as the original no.
+void print4(int n){
+    int duplicate = n; int reverseN = 0;
+    while(n > 0){
+        int lastDigit = n % 10;
+        n = n / 10;
+        reverseN = (reverseN * 10) + lastDigit;
+    }
+    if (reverseN == duolicate) cout<<"True";
+    else cout<<"False";
+}
+
+//Check Armstrong no.
+// addition of any power, of all the digits of a no. results in no.itself
+//eg -> 371 = 3 pow 3 + 7 pow 3 + 1 pow 3 = 371; 1634 = 1 pow 4 + 6 pow 4 + 3 pow + 4 pow 4
+void print5(int n){
+    int duplicate = n; int sum = 0;
+    while(n > 0){
+        int lastDigit = n % 10;
+        count ++;
+        sum = sum + pow(lastDigit, count);
+        n = n / 10;
+    }
+    if (sum == duplicate) cout<<"True";
+    else cout<<"False";
+}
+
+//Print all divisors
+//brute-force approach
+void print6(int n){
+    for(int i =0; i <= n; i++){
+        if(n % i == 0) cout<<i<<" ";
+    }
+}
+//optimized way
+void print7(int n) vector<int>ls{
+    for( int i =0; i*i <= n; i++){//n = 36, looping from 6*6 <= 36, so that we don't take no. that comes again
+        if(n % i == 0){//like 1 * 36 but not 36 * 1, 2* 18 but not 18 *2, etc
+            ls.push_back(i);
+
+            if(n/i != i){//we don't want to take 6 two times in 6*6=36
+            ls.push_back(n/i);//adding in list a no. which is not like repetetive
+        }
+        } 
+    }
+    sort(ls.begin(), ls.end());//sorting in order
+    for(auto it: ls) cout<<it<<" ";//printing it
 }
