96db50f1e37945c3120d2297818cc2857d6c2e05
thevaishndra <chandrav306m@gmail.com>
2024-07-11 19:50:31 +0530

    Update 04recursion.cpp

--- 04recursion.cpp
+++ 04recursion.cpp
@@ -22,10 +22,43 @@ void printNto1(int i, int n){
     printNto1(i-1, n);//3,3 => 2,3  1,3  0,3
 }
 
+//print 1 to n (backtrack)
+void print1toNbt(int i, int n){
+    if(i > n) return;
+    print1toNbt(i-1, n);//see what we are paassing 
+    cout<<i;//the last no. we will be printing first
+}
+
+//print n to 1 (backtrack)
+void printNto1bt(int i, int n){
+    if(i > n) return;
+    printNto1bt(i+1, n);
+    cout<<i;
+}
+
+//sum of first n no.
+//parameterised
+void sumOfNum(int i, int sum){
+    if(i < 1){
+        cout<<sum;
+        return;
+    }
+    sumOfNum(i - 1, sum + i);
+}
+
+//functional
+int sumOfNumFunc(){
+
+}
+
 int main(){
     int n;
     cin>>n;
     // printName(1, n);
     // print1toN(1, n);
     // printNto1(n, n);//passing 3,3
+    // print1toNbt(n, n);
+    // printNto1bt(1, n);
+    // sumOfNum(n, 0);
+    cout<<sumOfNumFunc();
 }
