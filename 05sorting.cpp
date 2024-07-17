f158881226067ff2650448d2915c9871cb90f347
thevaishndra <chandrav306m@gmail.com>
2024-07-18 04:13:14 +0530

    Update 05sorting.cpp

--- 05sorting.cpp
+++ 05sorting.cpp
@@ -31,4 +31,15 @@ void bubbleSort(int arr[], int n){
     }
 }
 
-//Insertion sort -> 
+//Insertion sort -> Takes an element and places it in its correct order
+void insertionSort (int arr[], int n){
+    for(int i = 0; i <= n - 1; i++){
+        int j = i;
+        while(j > 0 && arr[j - 1] > arr[j]){
+            int temp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+    }
+}
