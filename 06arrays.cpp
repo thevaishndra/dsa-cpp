8889c2f871e21dbe22240ec41f48483a1f998e96
thevaishndra <chandrav306m@gmail.com>
2024-09-20 00:30:08 +0530

    array basic ques

--- 06arrays.cpp
+++ 06arrays.cpp
@@ -32,3 +32,38 @@ int findSecondLargest(int arr[], int n){
 }
 //better
 
+
+//optimal
+int print2largest(vector<int> &arr) {
+        int largest = arr[0];
+        int slargest = -1;
+        
+        for (int i = 1; i < arr.size(); i++){
+            if(arr[i] > largest){
+                slargest = largest;
+                largest = arr[i];
+            } else if (arr[i] < largest && arr[i] > slargest){
+                slargest = arr[i];
+            } 
+        }
+        return slargest;
+        
+    }
+
+//Q3 Check if the array is sorted
+int checkSorted(int arr[], int n){
+    for(int i = 1; i < n; i++){
+        if(arr[i] >= arr[i-1]){
+            //we can leave it blank
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
+
+//Q4 Remove Duplicates from sorted array
+
+
+
+
