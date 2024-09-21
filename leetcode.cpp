d8bdde1d82ac97cfd686944b34e726e99dcb5fd7
thevaishndra <chandrav306m@gmail.com>
2024-09-21 19:33:20 +0530

    arrays

--- leetcode.cpp
+++ leetcode.cpp
@@ -20,3 +20,28 @@
 //             }
 //         }
 //         return i + 1;
+
+
+//189. Rotate Array
+//  k = k % nums.size();
+//         reverse(nums.begin(), nums.end());
+//         reverse(nums.begin(), nums.begin() + k);
+//         reverse(nums.begin() + k, nums.end());
+
+
+//283. Move Zeroes
+// int j = -1;
+//         for(int i = 0; i < nums.size(); i++){
+//             if(nums[i] == 0){
+//                 j = i;
+//                 break;
+//             }
+//         }
+//         if (j == -1) return;
+//         for(int i = j + 1; i < nums.size(); i++){
+//             if(nums[i] != 0){
+//                 swap(nums[i], nums[j]);
+//                 j++;
+//             }
+//         }
+//         return;
