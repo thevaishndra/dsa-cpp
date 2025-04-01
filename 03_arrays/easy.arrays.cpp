18722c00bfd19057e43de0aab5b958d2f9802d05
thevaishndra <chandrav306m@gmail.com>
2025-04-02 02:56:23 +0530

    arrays revision

--- 03_arrays/easy.arrays.cpp
+++ 03_arrays/easy.arrays.cpp
@@ -128,7 +128,7 @@ void leftRotateOne(int arr[], int n){
         cout<<arr[i]<<" ";//rotated array
     }
 }
-//tc - O(n) sc - O(1)
+//tc -> O(n) sc -> O(1)
 
 
 //Q6. Left rotate the array by D places
@@ -145,13 +145,14 @@ int leftRotateByD(int arr[], int n, int d){
     //     j++;
     // } But instead of writing j we can do
     for(int i = n - d; i < n; i++){//copy the elements from temp[] to the end of arr[]
-        arr[i] = temp[i - (n - d)];
+        arr[i] = temp[i - (n - d)];//indexing of temp - 0, 1, 2 - so that the elements on those index can be copied to array
     }
 }
+//tc -> O(1) + O(d) + O(n - d) + O(d) = O(n - d + d) => O(n + d)  sc -> O(d) + O(1) = O(d)
 
 //Optimal
 void reverse(int arr[], int start, int end){//Function to reverse elements if not using stl otherwise cpp stl has in-built reverse func
-    while(start <= end){
+    while(start <= end){//1,2,3,4,5 -> 5,2,3,4,1 -> 5,4,3,2,1 -> reverses in this way
         int temp = arr[start];
         arr[start] = arr[end];
         arr[end] = temp;
@@ -168,6 +169,8 @@ int leftRotateArrayByD2(int arr[], int n, int d){//function for rotation
   reverse(arr, 0, n - 1);
 }
 //in right rotation, reverse last d elements, first n - d elements, reverse whole array
+//tc -> O(d) + O(n - d) + O(n) = O(2n) sc -> O(1)
+//tc slightly increased
 
 
 //Q7. Move all zeroes to the end of array
@@ -191,6 +194,8 @@ int moveZeroesToEnd(int arr[], int n){
         cout<<arr[i]<<" ";
     }
 }
+//tc -> O(n) + O(x) + O(n - x) = O(2n) sc -> O(x) but O(n) in worst case
+
 //Optimal
 vector<int> moveZeroesToEnd2(int n, vector<int>arr){
     int j = -1;
@@ -204,11 +209,12 @@ vector<int> moveZeroesToEnd2(int n, vector<int>arr){
     for(int i = j + 1; i < n; i++){
         if(arr[i] != 0){
             swap(arr[i], arr[j]);//as we used vector we need not write code for swap
-            j++;
+            j++;//only values are swapped-> 0s are going towards left and j is still pointing at 0 after j++ whereas i is still at j + 1 after i++
         }
     }
     return arr;//we need not write a loop to print array
 }
+//tc -> O(x) +O(n - x) = O(n) sc -> O(1)
 
 
 //Q8. Linear search
@@ -220,6 +226,7 @@ int linearSearch(int arr[], int n, int num){
     }
       return -1;
 }
+//tc -> O(n) sc -> O(1)
 
 
 //Q9. Union of two sorted array
@@ -233,48 +240,89 @@ vector <int> findUnion(int arr1[], int arr2[], int n, int m){
         st.insert(arr2[i]);//add unique elements in set from array 2
     }
     vector <int> Union;
-    for(auto & it : st){
+    for(auto &it : st){//&it -> reference to each element
         Union.push_back(it);//now putting back set elements in the new array called union
     }
 }
+//tc -> O(n1 log n) + O(n2 log n) + O(n1 + n2) sc -> O(n1 + n2) + O(n1 + n2) - once to store, and other time to return the answer
+
 //Optimal
-vector <int> findUnion(int arr1[], int arr2[], int n, int m){
-    int  i = 0; int  j = 0;
-    vector <int> Union;
-    while( i < n && j < m){
-        if(arr1[i] <= arr2[j]){
-            if(Union.size() == 0 || Union.back() != arr1[i]){
+vector<int> findUnion2(int arr1[], int arr2[], int n, int m) {
+    int i = 0, j = 0;//i -> pointer for arr1; j -> pointer for arr 2
+    vector<int> Union;//a vector to store final union of both arrays
+    
+    while (i < n && j < m) {
+        if (arr1[i] <= arr2[j]) {//starting with arr1[i]
+            if (Union.empty() || Union.back() != arr1[i]) {//we check if union is empty or it is not equal to same element
                 Union.push_back(arr1[i]);
-                i++;
             }
-        }
-        else {
-            if(Union.size() == 0 || Union.back() != arr2[j]){
+            i++;//arr1[i] has been processed
+        } else {//starting with arr2[i]
+            if (Union.empty() || Union.back() != arr2[j]) {
                 Union.push_back(arr2[j]);
-                j++;
             }
+            j++;//arr2[j] has been processed
         }
     }
-    while(i < n){
-        if(Union.back() != arr1[i]){
+
+    while (i < n) {//remaining elements for arr1
+        if (Union.empty() || Union.back() != arr1[i]) {
             Union.push_back(arr1[i]);
-            i++;
         }
+        i++;
     }
-    while(j < m){
-        if(Union.back() != arr2[j]){
+
+    while (j < m) {//remaining elements for arr2
+        if (Union.empty() || Union.back() != arr2[j]) {
             Union.push_back(arr2[j]);
-            j++;
         }
+        j++;
     }
-    return Union;
+
+    return Union;//final union of botha arrays
 }
+//tc -> O(n + m) sc -> O(n + m) in worst case if no duplicates exist
 
 
 //Q10. Intersection of two sorted array
 //Brute
+vector<int> findIntersection(int arr1[], int arr2[], int n, int m) {
+    vector<int> intersection;//vector to store final ans
+    vector<int> visited(m, 0); //visited array to track which elements in arr2 have already been used
 
+    for (int i = 0; i < n; i++) {//we start from arr1[i] and traverse whole of arr2[j] to match elements
+        for (int j = 0; j < m; j++) {
+            if (arr1[i] == arr2[j] && visited[j] == 0) { //check if element is common and unvisited
+                intersection.push_back(arr1[i]);  //Add the element to the ans
+                visited[j] = 1;  // Mark this element in arr2 as visited, when we will agin traverse the whole array we will not take this
+                break;  
+            }
+            if (arr2[j] > arr1[i])  //how we are matching and traversing arr2, if arr[i] = 4 and we traversed arr2 and found element greater than 4 like 5,
+                break; //so we know we won't be having 4 ahead because it's a sorted array and we will break the loop
+        }
+    }
+    return intersection;
+}
+//tc -> O(n * m) sc -> O(n * m)
 
+//optimal
+vector<int> findIntersection2(int arr1[], int arr2[], int n, int m) {
+    vector <int> intersection;//vector that stores common elements between 2 arrays
+    int i = 0; int j = 0;
+    while(i < n && j < m){//within bounds
+        if(arr1[i] < arr2[j]){//if the element is lesser we will not take
+            i++;
+        } else if (arr2[j] < arr1[i]){//if the element is greater we will not take
+            j++;
+        } else {//if the elments are same we are adding them in intersection
+            intersection.push_back(arr1[i]);
+            i++;
+            j++;
+        }
+    }
+    return intersection;
+}
+//tc -> O(n + m) in worst case sc -> O(1) and O(n + m) in worst case if no intersections
 
 
 //Q11. Find missing no. in an array
