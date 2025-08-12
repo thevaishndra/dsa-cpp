db3c0e65f44de3ed878e1fef593cfea72520f8ba
thevaishndra <chandrav306m@gmail.com>
2025-08-12 13:45:47 +0530

    longest subarray sum k positives + negatives

--- 03_arrays/easy/longest-subarray-sumk-positives-negatives.cpp
+++ 03_arrays/easy/longest-subarray-sumk-positives-negatives.cpp
@@ -1,1 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
 //Q15. Longest subarray with sum K [positives + negatives]
+//Brute - same code when we used for longest subarray sum k positives
+int getLongestSubarray(vector<int>& arr, int k) {
+    int len = 0;
+    for (int i = 0; i < arr.size(); i++) { // starting index
+        for (int j = i; j < arr.size(); j++) { // ending index
+            // add all the elements of
+            // subarray = a[i...j]:
+            int s = 0;
+            for (int K = i; K <= j; K++) {
+                s += arr[K];
+            }
+
+            if (s == k)
+                len = max(len, j - i + 1);
+        }
+    }
+    return len;
+}
+
+//Optimal -> the better sol of longest subarray sum k positives
+int getLongestSubarray(vector<int>& a, int k) {
+    int n = a.size(); // size of the array.
+
+    map<int, int> preSumMap;
+    int sum = 0;
+    int maxLen = 0;
+    for (int i = 0; i < n; i++) {
+        //calculate the prefix sum till index i:
+        sum += a[i];
+
+        // if the sum = k, update the maxLen:
+        if (sum == k) {
+            maxLen = max(maxLen, i + 1);
+        }
+
+        // calculate the sum of remaining part i.e. x-k:
+        int rem = sum - k;
+
+        //Calculate the length and update maxLen:
+        if (preSumMap.find(rem) != preSumMap.end()) {
+            int len = i - preSumMap[rem];
+            maxLen = max(maxLen, len);
+        }
+
+        //Finally, update the map checking the conditions:
+        if (preSumMap.find(sum) == preSumMap.end()) {
+            preSumMap[sum] = i;
+        }
+    }
+
+    return maxLen;
+}
+//tc -> O(n*1) unordered map -> O(n*n) in worst case if it has collisions //sc -> O(n) worst case
--- 03_arrays/easy/longest-subarray-sumk-positives.cpp
+++ 03_arrays/easy/longest-subarray-sumk-positives.cpp
@@ -46,4 +46,33 @@ int longestSubarraySumKPos2(vector<int> &arr, long long K){
     }
     return maxLen;
 }
-//tc -> O(n) //sc -> O(1)
+//tc -> O(n) -> unordered map O(logn) -> O(nlogn) //sc -> O(1)
+
+//Optimal
+int getLongestSubarray(vector<int>& a, long long k) {
+    int n = a.size(); // size of the array.
+
+    int left = 0, right = 0; // 2 pointers
+    long long sum = a[0];
+    int maxLen = 0;
+    while (right < n) {
+        // if sum > k, reduce the subarray from left
+        // until sum becomes less or equal to k:
+        while (left <= right && sum > k) {
+            sum -= a[left];
+            left++;
+        }
+
+        // if sum = k, update the maxLen i.e. answer:
+        if (sum == k) {
+            maxLen = max(maxLen, right - left + 1);
+        }
+
+        // Move forward thw right pointer:
+        right++;
+        if (right < n) sum += a[right];
+    }
+
+    return maxLen;
+}
+//tc -> O(2n) //sc -> O(1)
