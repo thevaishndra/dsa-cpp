43c304912007c06940ca5cec176705e3c2e0772f
thevaishndra <chandrav306m@gmail.com>
2025-11-22 22:16:22 +0530

    bit manipulation

--- 06_bitManipulation/intro/01-basics.cpp
+++ 06_bitManipulation/intro/01-basics.cpp
@@ -27,7 +27,7 @@ string convert2Binary(int n) {
 
 //2. Conersion Binary base(2) to Decimal base(10)
 //start with right most bit called as least significant bit
-//Each bit is multiplied by 2 raised to the power of its position index.
+//Each bit is multiplied by 2 raised to the power of its position index
 //1 * 2^0 + 1 * 2^1 + 1 * 2^2 = 1 + 2 + 4 => 7
 //decimal of 111 is 7
 
@@ -47,22 +47,38 @@ int convert2Decimal (string x) {
 //tc -> O(len) //sc -> O(1)
 
 //3. Ones complement
-//The one's complement of a binary number is obtained by flipping all the bits.
+//The one's complement of a binary number is obtained by flipping all the bits
 //(1101) base2 -> (0010) base2
 
 
 //4. Twos complement
-//The two's complement is obtained by taking the one's complement of a number and adding 1.
+//The two's complement is obtained by taking the one's complement of a number and adding 1
 //1s complement + 1 -> 0010 + 1 -> (0011) base2
 
 //5. Bitwise Operators
 // AND Operator (&) -> all true is true; 1 false is false
 //1101 & 0111 -> 0101
 
-// OR Operator (|) -> 1 true is true; all false is false
+//6. OR Operator (|) -> 1 true is true; all false is false
 //1101 | 0111 -> 1111
 
-//XOR Operator (^) -> if no. of 1s is odd then 1; if no. of 1s is odd then 0
+//7. XOR Operator (^) -> if no. of 1s is odd then 1; if no. of 1s is odd then 0
 //1101 ^ 0111 -> 1010
 
-//Shift
+//8. NOT Operator (~) -> Flips all bits of the number
+//so, to do x = ~(s) -> 1.flip  2.Check -ve  3.If yes then Add 1 (2s complement) 3. Add -ve sign
+//~5 -> ~0101 -> 1010 (It becomes a -ve no as msb is 1)
+//1010 -> 0101 -> 0101 + 1 -> 0110 => 6 add -sign from og val => -6
+//~(-6) -> ~0110 -> 1001 -> 1001 + 1 -> 1010 -> 5
+
+//9. binary addition 1+1 = 10 -> write 0 carry 1; 1+1+1=11 -> write 1 carry 1
+
+//10. Shift
+//Right shift -> Shifts bits to the right, fills left with 0s
+//13 >> 1 -> 1101 >> 1 -> 110 => 6  //13 >> 4 -> 1101 >> 4 -> 0000 => 0
+//Left shift -> Shifts bits to the left, fills right with 0s
+//13 << 1 -> 1101 << 1 -> 11010 => 16 + 8 + 2 = 26 (left shift by 1 is same as multiplying by 2)
+//13 << 4 -> 1101 << 4 -> 11010000 => 16 + 64 + 128 => 208
+
+
+//swap 2 no. without a third variable
