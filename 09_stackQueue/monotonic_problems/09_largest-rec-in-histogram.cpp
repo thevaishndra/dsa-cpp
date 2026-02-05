fee5c6a41920db82b2e8ff372a01d9876fdfc81a
thevaishndra <chandrav306m@gmail.com>
2026-02-05 21:04:47 +0530

    largest rectangle histogram

--- 09_stackQueue/monotonic_problems/09_largest-rec-in-histogram.cpp
+++ 09_stackQueue/monotonic_problems/09_largest-rec-in-histogram.cpp
@@ -1,43 +1,86 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//Q. Largest Rectangle in histogram
 // Given an array of integers heights representing the histogram's bar height where the width of each bar is 1 return the area of the largest rectangle in histogram
 //Brute
-func(arr) {
-    nse = findNSE(arr);
-    pre = findNSE(Arr);
-    maxi = 0;
+vector<int> findPSE(vector<int>& arr) {
+    int n = arr.size();
+    vector<int> pse(n);
+    stack<int> st;
 
-    for(int i = 0; i < n - 1; i++) {
-        maxi = max(maxi, arr[i] * (nse[i] - pse[i] - 1))
+    for (int i = 0; i < n; i++) {
+        while (!st.empty() && arr[st.top()] >= arr[i]) {
+            st.pop();
+        }
+        pse[i] = st.empty() ? -1 : st.top();
+        st.push(i);
+    }
+    return pse;
+}
+
+vector<int> findNSE(vector<int>& arr) {
+    int n = arr.size();
+    vector<int> nse(n);
+    stack<int> st;
+
+    for (int i = n - 1; i >= 0; i--) {
+        while (!st.empty() && arr[st.top()] >= arr[i]) {
+            st.pop();
+        }
+        nse[i] = st.empty() ? n : st.top();
+        st.push(i);
+    }
+    return nse;
+}
+
+int largestRectangleArea(vector<int>& arr) {
+    int n = arr.size();
+    vector<int> pse = findPSE(arr);
+    vector<int> nse = findNSE(arr);
+
+    int maxi = 0;
+
+    for (int i = 0; i < n; i++) {
+        int width = nse[i] - pse[i] - 1;
+        maxi = max(maxi, arr[i] * width);
     }
     return maxi;
 }
+
 //tc -> O(5n) //sc -> O(2n)
 
 
 //Optimal
-func(Arr) {
-    stack st;
-    maxArea = 0;
+int largestRectangleArea(vector<int>& arr) {
+    int n = arr.size();
+    stack<int> st;
+    int maxArea = 0;
 
-    for(int i = 0; i < n - 1; i++) {
-        while(st.empty() && arr[st.top] > arr[i]) {
-            element = st.top();
+    for (int i = 0; i < n; i++) {
+        while (!st.empty() && arr[st.top()] > arr[i]) {
+            int element = st.top();
             st.pop();
-            nse = i;
-            pse = st.empty() ? - 1 : st.top();
-            maxArea = max(arr[element] * (nse - pse - 1), maxArea);
+
+            int nse = i;
+            int pse = st.empty() ? -1 : st.top();
+
+            maxArea = max(maxArea, arr[element] * (nse - pse - 1));
         }
-        st.push[i];
+        st.push(i);
     }
 
-    while(!st.empty()) {
-        nse = n;
-        element = st.top(); st.pop();
-        pse = st.empty() ? - 1 : st.top;
-        maxArea = max(maxArea, (nse - pse - 1) * arr[ele])
+    while (!st.empty()) {
+        int element = st.top();
+        st.pop();
+
+        int nse = n;
+        int pse = st.empty() ? -1 : st.top();
+
+        maxArea = max(maxArea, arr[element] * (nse - pse - 1));
     }
+
     return maxArea;
 }
+
 //tc -> O(n) + O(n) //sc -> O(n)
